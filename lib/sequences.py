71af6c197ce471dc9c0e919e69f99814842784d2
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-11-22 11:09:29 -0500

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    list = []
+    if length > 0:
+        list.append(0)
+        if length > 1:
+            list.append(1)
+    for i in range(2, length):
+        last_two_sum = list[-1] + list[-2]
+        list.append(last_two_sum)
+    print(list)
+    
